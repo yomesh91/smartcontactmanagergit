a61c650587d7d582a7b18167090762776578b855
yomesh91 <yomeshmodi91@gmail.com>
2020-11-10 22:03:36 +0530

    commited10

--- src/main/java/com/smart/smartcontactmanager/controller/UserController.java
+++ src/main/java/com/smart/smartcontactmanager/controller/UserController.java
@@ -4,13 +4,17 @@ import com.smart.smartcontactmanager.dao.UserRepository;
 import com.smart.smartcontactmanager.entities.Contact;
 import com.smart.smartcontactmanager.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.security.Principal;
 
 @Controller
@@ -18,32 +22,56 @@ import java.security.Principal;
 public class UserController {
     @Autowired
     private UserRepository userRepository;
+
     @ModelAttribute
-    public void addCommonData(Model model,Principal principal){
+    public void addCommonData(Model model, Principal principal) {
         String email = principal.getName();
         User user = userRepository.getUserByUserName(email);
-        model.addAttribute("user",user);
+        model.addAttribute("user", user);
 
     }
+
     @RequestMapping("/index")
-    public String dashboard(Model model, Principal principal){
-        model.addAttribute("title","Home");
-       return "normal/user_dashboard";
+    public String dashboard(Model model, Principal principal) {
+        model.addAttribute("title", "Home");
+        return "normal/user_dashboard";
     }
+
     @GetMapping("/add-contact")
-    public String addContact(Model model){
-        model.addAttribute("title","Add Contact");
-        model.addAttribute("contact",new Contact());
+    public String addContact(Model model) {
+        model.addAttribute("title", "Add Contact");
+        model.addAttribute("contact", new Contact());
         return "normal/add_contact";
     }
+
     @PostMapping("/register-contact")
-    public  String registerContact(@ModelAttribute Contact contact,Principal principal){
-        String email = principal.getName();
-        User user = userRepository.getUserByUserName(email);
-        contact.setUser(user);
-        user.getContacts().add(contact);
-        userRepository.save(user);
-        System.out.println(contact);
+    public String registerContact(
+            @ModelAttribute Contact contact,
+            @RequestParam("profileImage") MultipartFile file,
+            Principal principal) {
+        try {
+            String email = principal.getName();
+            User user = userRepository.getUserByUserName(email);
+            //file uploding
+            if (file.isEmpty()) {
+                System.out.println("file is empty");
+
+            } else {
+                contact.setImage(file.getOriginalFilename());
+                File file1 = new ClassPathResource("/static/images").getFile();
+                Path path = Paths.get(file1.getAbsolutePath() + File.separator + file.getOriginalFilename());
+                Files.copy(file.getInputStream(),path, StandardCopyOption.REPLACE_EXISTING);
+                System.out.println("Image is uploaded");
+            }
+            contact.setUser(user);
+            user.getContacts().add(contact);
+            userRepository.save(user);
+            System.out.println(contact);
+            System.out.println("Added to database");
+        } catch (Exception e) {
+            System.out.println("Error " + e.getMessage());
+            e.printStackTrace();
+        }
         return "normal/add_contact";
     }
 
