1c09d2ffaad139e80670344ab4ae56af336202d5
yomesh91 <yomeshmodi91@gmail.com>
2020-11-20 17:58:07 +0530

    commited10

--- src/main/java/com/smart/smartcontactmanager/controller/UserController.java
+++ src/main/java/com/smart/smartcontactmanager/controller/UserController.java
@@ -17,6 +17,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpSession;
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -71,35 +72,37 @@ public class UserController {
                 contact.setImage(file.getOriginalFilename());
                 File file1 = new ClassPathResource("/static/images").getFile();
                 Path path = Paths.get(file1.getAbsolutePath() + File.separator + file.getOriginalFilename());
-                Files.copy(file.getInputStream(),path, StandardCopyOption.REPLACE_EXISTING);
+                Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
                 System.out.println("Image is uploaded");
             }
             contact.setUser(user);
             user.getContacts().add(contact);
             userRepository.save(user);
             System.out.println("Added to database");
-            session.setAttribute("message", new Message("Your Contact is Added Successfully..","success"));
+            session.setAttribute("message", new Message("Your Contact is Added Successfully..", "success"));
         } catch (Exception e) {
             System.out.println("Error " + e.getMessage());
             e.printStackTrace();
-            session.setAttribute("message", new Message("Something Went Wrong..","danger"));
+            session.setAttribute("message", new Message("Something Went Wrong..", "danger"));
         }
         return "normal/add_contact";
     }
+
     @GetMapping("/show-contacts/{page}")
-    public String showContacts(@PathVariable("page") Integer page, Model model,Principal principal){
-        model.addAttribute("title","Show User contacts");
+    public String showContacts(@PathVariable("page") Integer page, Model model, Principal principal) {
+        model.addAttribute("title", "Show User contacts");
         String email = principal.getName();
         User user = userRepository.getUserByUserName(email);
         Pageable pageable = PageRequest.of(page, 3);
-        Page<Contact> contacts = contactRepositry.findContactByUser(user.getId(),pageable);
-        model.addAttribute("contacts",contacts);
-        model.addAttribute("currentPage",page);
-        model.addAttribute("totalPages",contacts.getTotalPages());
+        Page<Contact> contacts = contactRepositry.findContactByUser(user.getId(), pageable);
+        model.addAttribute("contacts", contacts);
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalPages", contacts.getTotalPages());
         return "normal/show_contacts";
     }
+
     @GetMapping("/contact/{conId}")
-    public String showContactDetailById(@PathVariable("conId") Integer conId,Model model,Principal principal){
+    public String showContactDetailById(@PathVariable("conId") Integer conId, Model model, Principal principal) {
         Optional<Contact> optionalContact = contactRepositry.findById(conId);
         Contact contact = optionalContact.get();
 
@@ -107,22 +110,69 @@ public class UserController {
         User user = userRepository.getUserByUserName(email);
         if (user.getId() == contact.getUser().getId()) {
             model.addAttribute("contact", contact);
-            model.addAttribute("title",contact.getName());
+            model.addAttribute("title", contact.getName());
         }
-            return "normal/contact_detail";
+        return "normal/contact_detail";
     }
+
     @GetMapping("/delete/{conId}")
-    public String deleteContact(@PathVariable("conId") Integer conId,Model model,Principal principal, HttpSession session){
+    public String deleteContact(@PathVariable("conId") Integer conId, Model model, Principal principal, HttpSession session) throws IOException {
         Optional<Contact> optionalContact = contactRepositry.findById(conId);
         Contact contact = optionalContact.get();
 
         String email = principal.getName();
         User user = userRepository.getUserByUserName(email);
-        if(user.getId() == contact.getUser().getId()) {
+        if (user.getId() == contact.getUser().getId()) {
+            File deleteFile = new ClassPathResource("/static/images").getFile();
+            File file3 = new File(deleteFile,contact.getImage());
+            file3.delete();
             contactRepositry.delete(contact);
             session.setAttribute("message", new Message("Contact deleted succesfully", "success"));
         }
         return "redirect:/user/show-contacts/0";
+    }
+
+    @PostMapping("/update-contact/{conId}")
+    public String updateForm(@PathVariable("conId") Integer conId, Model model) {
+
+        Contact contact = contactRepositry.findById(conId).get();
+        model.addAttribute("title", "Update Contact");
+        model.addAttribute("contact", contact);
+        return "normal/update_form";
+    }
+
+    @PostMapping("/updated-contact")
+    public String updated(@ModelAttribute Contact contact, @RequestParam("profileImage") MultipartFile file, Model model, HttpSession session, Principal principal) {
+        try {
+            //old contact detail
+            Contact oldContactDetail = contactRepositry.findById(contact.getConId()).get();
+            //image...
+            if (!file.isEmpty()) {
+                //delete photo
+                File deleteFile = new ClassPathResource("/static/images").getFile();
+                File file3 = new File(deleteFile,oldContactDetail.getImage());
+                file3.delete();
+                //update new photo
+                File file1 = new ClassPathResource("/static/images").getFile();
+                Path path = Paths.get(file1.getAbsolutePath() + File.separator + file.getOriginalFilename());
+                Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+                contact.setImage(file.getOriginalFilename());
+            }else{
+                contact.setImage(oldContactDetail.getImage());
+            }
+            User user = userRepository.getUserByUserName(principal.getName());
+            contact.setUser(user);
+            contactRepositry.save(contact);
+            session.setAttribute("message",new Message("Your contact is updated!!!","success"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "redirect:/user/contact/"+contact.getConId();
+    }
+
+    @GetMapping("/profile")
+    public String profile(Model model){
+        return "normal/profile";
     }
 
 }
